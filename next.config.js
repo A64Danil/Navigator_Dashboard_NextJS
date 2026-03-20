9036836e0ceb1c1b7def58c2318e3624bc183416
DanilZen <a64danil@mail.ru>
2026-03-20 17:54:00 +0100

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/Navigator_Dashboard_NextJS',
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
