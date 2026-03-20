bd37693cf6bf21d682a7ebfac3023aaeba3822a3
DanilZen <a64danil@mail.ru>
2026-03-20 16:42:48 +0100

    feat: add github pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/Navigator_Dashboard_NextJS',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
